64ac9671966fea466782297f049947b8f99681d4
Stepan Gerasimov <mr.gerasimov.stepan@yandex.ru>
2024-07-19 02:10:18 +0300

    еще правки вьюшек

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -215,7 +215,12 @@ class TitleViewSet(viewsets.ModelViewSet):
     def partial_update(self, request, *args, **kwargs):
         if request.user.role == 'user' or request.user.role == 'moderator':
             return Response(status=status.HTTP_403_FORBIDDEN)
-        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def get_serializer_class(self):
         if self.action in ['create', 'update', 'partial_update']:
