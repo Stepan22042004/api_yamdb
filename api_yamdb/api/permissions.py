48ef909625131014df5d5f0cee727cc7820b2774
Stepan Gerasimov <mr.gerasimov.stepan@yandex.ru>
2024-07-19 23:06:07 +0300

    codestyle

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -12,17 +12,17 @@ class IsAuthorOrModeratorOrAdmin(BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return (
-            request.method in SAFE_METHODS or
-            obj.author == request.user or
-            request.user.is_staff or
-            request.user.is_superuser
+            request.method in SAFE_METHODS
+            or obj.author == request.user
+            or request.user.is_staff
+            or request.user.is_superuser
         )
 
 
 class IsAdminUser(BasePermission):
     def has_permission(self, request, view):
-        return request.user and (request.user.role == 'admin' or
-                                 request.user.is_staff)
+        return request.user and (request.user.role == 'admin'
+                                 or request.user.is_staff)
 
 
 class IsAdminOrReadOnly(BasePermission):
