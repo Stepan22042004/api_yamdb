415326178e9905a27d580491b05ca49a5e31bb8d
Stepan Gerasimov <mr.gerasimov.stepan@yandex.ru>
2024-07-19 23:51:58 +0300

    codestyle

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -22,10 +22,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'reviews',
+    'reviews.apps.ReviewsConfig',
     'api',
     'rest_framework',
-    'rest_framework_simplejwt',
     'django_filters',
 ]
 
