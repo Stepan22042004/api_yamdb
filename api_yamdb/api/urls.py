352f97aea6f2e7e40d65a6600baa601e1d995e64
Anastasia Chugunova <sc0rpiosha11@yandex.ru>
2024-07-21 23:04:50 +0300

    Исправлены замечания со скринов 5-7

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -6,25 +6,29 @@ from api.views import (CategoryViewSet, CommentViewSet, GenreViewSet,
                        TokenObtainView, AdminRegisterViewSet,
                        UserProfileView)
 
-router = DefaultRouter()
-router.register('users', AdminRegisterViewSet, basename='users')
-router.register('categories', CategoryViewSet, basename='category')
-router.register('genres', GenreViewSet, basename='genre')
-router.register('titles', TitleViewSet, basename='title')
-router.register(
+router_v1 = DefaultRouter()
+router_v1.register('users', AdminRegisterViewSet, basename='users')
+router_v1.register('categories', CategoryViewSet, basename='category')
+router_v1.register('genres', GenreViewSet, basename='genre')
+router_v1.register('titles', TitleViewSet, basename='title')
+router_v1.register(
     r'titles/(?P<title_id>\d+)/reviews',
     ReviewViewSet,
     basename='review'
 )
-router.register(
+router_v1.register(
     r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
     CommentViewSet,
     basename='comment'
 )
 
+api_v1_auth_urls = [
+    path('users/me/', UserProfileView.as_view(), name='user-profile'),
+    path('auth/signup/', UserRegisterView.as_view(), name='signup'),
+    path('auth/token/', TokenObtainView.as_view(), name='token_obtain')
+]
+
 urlpatterns = [
-    path('v1/users/me/', UserProfileView.as_view(), name='user-profile'),
-    path('v1/auth/signup/', UserRegisterView.as_view(), name='signup'),
-    path('v1/auth/token/', TokenObtainView.as_view(), name='token_obtain'),
-    path('v1/', include(router.urls)),
+    path('v1/', include(api_v1_auth_urls)),
+    path('v1/', include(router_v1.urls))
 ]
