c70442f045cde98b536e1c0aa677dd662db426b3
Stepan Gerasimov <mr.gerasimov.stepan@yandex.ru>
2024-07-25 00:52:30 +0300

    final

--- api_yamdb/reviews/validators.py
+++ api_yamdb/reviews/validators.py
@@ -1,9 +1,19 @@
 from django.core.exceptions import ValidationError
+from django.utils import timezone
 
 
 def validate_username(value):
     if value.lower() == 'me':
         raise ValidationError(
             '%(value)s is not a valid username',
+            params={'value': value},
+        )
+
+
+def validate_year(value):
+    current_year = timezone.now().year
+    if value > current_year:
+        raise ValidationError(
+            f'Year can not be more than {current_year}.',
             params={'value': value},
         )
