34a24f5e633e49d34f6f5c1b3fe65d32b7f31a39
Anastasia Chugunova <sc0rpiosha11@yandex.ru>
2024-07-24 12:04:39 +0300

    Поправлен фильтр

--- api_yamdb/api/filters.py
+++ api_yamdb/api/filters.py
@@ -14,11 +14,6 @@ class TitleFilter(django_filters.FilterSet):
         lookup_expr='iexact'
     )
 
-    year = django_filters.NumberFilter(
-        field_name='year',
-        lookup_expr='exact'
-    )
-
     name = django_filters.CharFilter(
         field_name='name',
         lookup_expr='icontains'
